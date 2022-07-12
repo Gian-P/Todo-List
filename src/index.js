913ed0a47956701dbafa96a31412241a7b19368f
Gian-P <pimentel.giancarlo1@gmail.com>
2022-07-11 23:32:21 -0400

    Created the structure for the proyect

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import "./styles.css";
-import "./js/index.js";
+import {title} from './js/prueba.js';
+title();
+/*
+import "./styles.css";
+import "./js/index.js";*/
+/*This is the entry point for your js bundle */
 
 
 
