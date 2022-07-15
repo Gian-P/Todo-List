d149dd3e6c3e65c50bc870ff5678eb6a6c4c4857
Gian <pimentel.giancarlo1@gmail.com>
2022-07-15 14:56:43 -0400

    Finished the HTML structure for this proyect

--- src/js/prueba.js
+++
@@ -1,9 +0,0 @@
-function title(){
-  let title = document.getElementById("CONTENT");
-  let h3 = document.createElement("h3");
-  h3.className = "hello";
-  h3.textContent = "HELLO!";
-  title.appendChild(h3);
-}
-
-export {title};
